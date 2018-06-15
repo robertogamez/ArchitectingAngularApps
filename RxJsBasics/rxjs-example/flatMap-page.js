80e9ddced9c9c8bc01a5caf891ace9de4aa0bd3b
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-06-15 10:15:54 -0500

    Aprendiendo mas sobre operadores

--- RxJsBasics/rxjs-example/flatMap-page.js
+++ RxJsBasics/rxjs-example/flatMap-page.js
@@ -1,20 +1,37 @@
 ﻿let elem = document.getElementById('input');
 
-let keyStream$ = Rx.Observable
-    .fromEvent(elem, 'keyup')
-    .map(ev => ev.key)
-    .filter(key => key !== 'Backspace')
-    .flatMap(key => Rx.Observable.from(getStarwarsCharacterStream(key)));
+//let keyStream$ = Rx.Observable
+//    .fromEvent(elem, 'keyup')
+//    .map(ev => ev.key)
+//    .filter(key => key !== 'Backspace')
+//    .flatMap(key => Rx.Observable.from(getStarwarsCharacterStream(key)));
 
-keyStream$.subscribe(key => console.log(key));
+//keyStream$.subscribe(key => console.log(key));
 
-function getStarwarsCharacterStream(id) {
-    return fetch('data' + id + '' + '.json', {
-        'method': 'get',
-        'content-type': 'application/json'
-    })
-    .then(data => data.json());
+//function getStarwarsCharacterStream(id) {
+//    return fetch('data' + id + '' + '.json', {
+//        'method': 'get',
+//        'content-type': 'application/json'
+//    })
+//    .then(data => data.json());
+//}
+let keyStream$ = Rx.Observable.fromEvent(elem, 'keyup');
+
+function longRunningTask(input) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('response based on ' + input);
+        }, 5000);
+    });
 }
+
+let longRunningStream$ = keyStream$.map(ev => ev.key)
+                            .filter(key => elem.value.length > 3)
+                            .filter(key => key !== 'Backspace')
+                            .flatMap(key => Rx.Observable.from(longRunningTask(elem.value))
+                            );
+
+longRunningStream$.subscribe(data => console.log(data));
 
 //fetch('data.json', {
 //    'method': 'get',
---
+++ RxJsBasics/rxjs-example/switchMap-page.js
@@ -0,0 +1,19 @@
+﻿let elem = document.getElementById('input');
+
+let keyStream$ = Rx.Observable.fromEvent(elem, 'keyup');
+
+function longRunningTask(input) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('response based on ' + input);
+        }, 5000);
+    });
+}
+
+let longRunningStream$ = keyStream$.map(ev => ev.key)
+                            .filter(key => elem.value.length > 3)
+                            .filter(key => key !== 'Backspace')
+                            .switchMap(key => Rx.Observable.from(longRunningTask(elem.value))
+                            );
+
+longRunningStream$.subscribe(data => console.log(data));
