c7c484f9693e0f1a0dabe8fa6cbd9943a0f229ec
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-06-20 10:29:48 -0500

    Aprendiendo sobre observables frios y calientes

---
+++ RxJsBasics/hot-cold-warm/promise.js
@@ -0,0 +1,8 @@
+﻿function getData() {
+    return new Promise(resolve => {
+        console.log("this will be printed straight away");
+        setTimeout(() => resolve("som data"), 3000);
+    });
+}
+
+getData().then(data => console.log('3 seconds later', data));
