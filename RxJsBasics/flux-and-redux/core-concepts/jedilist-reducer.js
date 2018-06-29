5f1a0a3a3d0c885c3636175cda4734fff68e9cea
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-06-29 12:09:17 -0500

    Se iniciar con los principios principales de Redux

---
+++ RxJsBasics/flux-and-redux/core-concepts/jedilist-reducer.js
@@ -0,0 +1,29 @@
+﻿let actionLuke = {
+    type: 'ADD_ITEM',
+    payload: {
+        name: 'Luke'
+    }
+};
+
+
+let actionVader = {
+    type: 'ADD_ITEM',
+    payload: "Vader"
+};
+
+function jediListReducer(state = {}, action){
+    switch(action.type){
+        case "ADD_ITEM":
+            return [...state, action.payload]
+        default:
+            return state;
+    }
+}
+
+let state = jediListReducer([], actionLuke);
+console.log(state);
+
+state = jediListReducer(state, actionVader);
+console.log(state);
+
+module.exports = jediListReducer;
---
+++ RxJsBasics/flux-and-redux/core-concepts/selectjedi-reducer.js
@@ -0,0 +1,30 @@
+﻿let actionPerson = {
+    type: 'SELECT_JEDI',
+    payload: {
+        id: 1, name: 'Luke'
+    }
+};
+let actionVader = {
+    type: 'SELECT_JEDI',
+    payload: {
+        id: 2,
+        name: 'Vader'
+    }
+};
+
+function selectJediReducer({}, action){
+    switch (action.type) {
+        case "SELECT_JEDI":
+            return Object.assign({}, action.payload);
+        default:
+            return state;
+    }
+}
+
+state = selectJediReducer({}, actionPerson);
+console.log(state);
+
+state = selectJediReducer(state, actionVader);
+console.log(state);
+
+module.exports = selectJediReducer;
